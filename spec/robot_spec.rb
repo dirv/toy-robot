fdcc2780fe77daa95d968c119aabf3427ac4e56e
Daniel Irvine <daniel@danielirvine.com>
2025-04-30 15:46:54 +0100

    Pull out the robot_at test helper

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -12,6 +12,7 @@ describe "Robot" do
     end
 
   describe "#perform_action" do
+
     it "returns the same robot if the command is not recognised" do
       robot = Robot.new
       expect(robot.perform_action(:unknown)).to eq robot
@@ -24,43 +25,47 @@ describe "Robot" do
       expect(robot.facing).to eq :north
     end
 
+    def robot_at(position)
+      Robot.new.perform_action(:place, position)
+    end
+
     it "moves north" do
-      robot = Robot.new.perform_action(:place, { x: 1, y: 2, facing: :north })
+      robot = robot_at({ x: 1, y: 2, facing: :north })
       expect(robot.perform_action(:move).y).to eq 3
     end
 
     it "moves south" do
-      robot = Robot.new.perform_action(:place, { x: 1, y: 2, facing: :south })
+      robot = robot_at({ x: 1, y: 2, facing: :south })
       expect(robot.perform_action(:move).y).to eq 1
     end
 
     it "moves east" do
-      robot = Robot.new.perform_action(:place, { x: 1, y: 2, facing: :east })
+      robot = robot_at({ x: 1, y: 2, facing: :east })
       expect(robot.perform_action(:move).x).to eq 2
     end
 
     it "moves west" do
-      robot = Robot.new.perform_action(:place, { x: 1, y: 2, facing: :west })
+      robot = robot_at({ x: 1, y: 2, facing: :west })
       expect(robot.perform_action(:move).x).to eq 0
     end
 
     it "does not move north if the toy is at the north boundary" do
-      robot = Robot.new.perform_action(:place, { x: 1, y: 4, facing: :north })
+      robot = robot_at({ x: 1, y: 4, facing: :north })
       expect(robot.perform_action(:move).y).to eq 4
     end
 
     it "does not move south if the toy is at the south boundary" do
-      robot = Robot.new.perform_action(:place, { x: 1, y: 0, facing: :south })
+      robot = robot_at({ x: 1, y: 0, facing: :south })
       expect(robot.perform_action(:move).y).to eq 0
     end
 
     it "does not move east if the toy is at the east boundary" do
-      robot = Robot.new.perform_action(:place, { x: 4, y: 2, facing: :east })
+      robot = robot_at({ x: 4, y: 2, facing: :east })
       expect(robot.perform_action(:move).x).to eq 4
     end
 
     it "does not move west if the toy is at the west boundary" do
-      robot = Robot.new.perform_action(:place, { x: 0, y: 2, facing: :west })
+      robot = robot_at({ x: 0, y: 2, facing: :west })
       expect(robot.perform_action(:move).x).to eq 0
     end
 
@@ -73,7 +78,7 @@ describe "Robot" do
 
     LEFT_TURNS.each do |(start, finish)|
       it "turns left to face #{finish} when currently #{start}" do
-        robot = Robot.new.perform_action(:place, { x: 0, y: 2, facing: start })
+        robot = robot_at({ x: 0, y: 2, facing: start })
         expect(robot.perform_action(:left).facing).to eq finish
       end
     end
@@ -87,7 +92,7 @@ describe "Robot" do
 
     RIGHT_TURNS.each do |(start, finish)|
       it "turns right to face #{finish} when currently #{start}" do
-        robot = Robot.new.perform_action(:place, { x: 0, y: 2, facing: start })
+        robot = robot_at({ x: 0, y: 2, facing: start })
         expect(robot.perform_action(:right).facing).to eq finish
       end
     end
